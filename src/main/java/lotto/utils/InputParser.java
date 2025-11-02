eafc5bcdcc31745a4b2af8791903dd2e87cd14e0
clainyun <dainreason0617@gmail.com>
2025-11-02 18:32:35 +0900

    feat(InputParser): 당첨 번호 및 보너스 번호 파싱 기능 구현

---
+++ src/main/java/lotto/utils/InputParser.java
@@ -0,0 +1,30 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputParser {
+
+    public static List<Integer> parseWinningNumbers(String input) {
+        try {
+            List<Integer> winningNumbers = Arrays.stream(input.split(",")).map(String::trim).map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            if (winningNumbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            }
+            return winningNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 입력해야 합니다.");
+        }
+    }
+
+    public static int parseBonusNumber(String input) {
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+    }
+}
