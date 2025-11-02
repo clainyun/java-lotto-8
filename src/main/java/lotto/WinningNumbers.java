c2060fb0fdbdfe17097e495b33d6b709ec64e4c3
clainyun <dainreason0617@gmail.com>
2025-11-02 20:53:05 +0900

    feat(WinningNumbers): 보너스 번호 중복 및 범위 검증 기능 구현

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningLotto = new Lotto(winningNumbers);
+        validateBonusNumber(bonusNumber, winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
