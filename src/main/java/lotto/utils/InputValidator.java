f2ee6eeef5825550cef5f2482b95e24397a332fb
clainyun <dainreason0617@gmail.com>
2025-11-02 16:21:16 +0900

    feat(InputView, InputValidator): 구입 금액 입력 및 검증 기능 구현

---
+++ src/main/java/lotto/utils/InputValidator.java
@@ -0,0 +1,18 @@
+package lotto.utils;
+
+public class InputValidator {
+    private static final int LOTTO_PRICE = 1000;
+
+    public static int validatePurchaseAmount(String input) {
+        try {
+            int amount = Integer.parseInt(input);
+
+            if (amount % LOTTO_PRICE != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+    }
+}
