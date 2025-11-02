50348569d8a00c92e5538f5bfc4d5012cf61ca49
clainyun <dainreason0617@gmail.com>
2025-11-03 01:28:41 +0900

    docs(InputView): 입력 요청 메서드 주석 추가 및 가독성 개선

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,17 +3,21 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
+    // 구입 금액 입력
     public static String readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
+    // 당첨 번호 입력
     public static String readWinningNumbers() {
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
+    // 보너스 번호 입력
     public static String readBonusNumber() {
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
