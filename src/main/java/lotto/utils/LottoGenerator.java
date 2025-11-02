3b64b6ea26323fc0b7aeab489a368cdc4eba38f6
clainyun <dainreason0617@gmail.com>
2025-11-03 01:21:26 +0900

    refactor(LottoGenerator): 단일 로또 생성 메서드 분리로 SRP 원칙 반영

--- src/main/java/lotto/utils/LottoGenerator.java
+++ src/main/java/lotto/utils/LottoGenerator.java
@@ -3,7 +3,6 @@ package lotto.utils;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import lotto.Lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -13,16 +12,21 @@ public class LottoGenerator {
     private static final int LOTTO_MAX = 45;
     private static final int LOTTO_COUNT = 6;
 
+    // 전체 로또 발행 (구입 금액 기준)
     public static List<Lotto> generateLottos(int purchaseAmount) {
         int count = purchaseAmount / LOTTO_PRICE;
         List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = new ArrayList<>(
-                    Randoms.pickUniqueNumbersInRange(LOTTO_MIN, LOTTO_MAX, LOTTO_COUNT));
-            Collections.sort(numbers);
-            lottos.add(new Lotto(numbers));
+            lottos.add(new Lotto(generateSingleLottoNumbers()));
         }
         return lottos;
+    }
+
+    // 단일 로또 번호 생성 및 정렬
+    private static List<Integer> generateSingleLottoNumbers() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(LOTTO_MIN, LOTTO_MAX, LOTTO_COUNT));
+        Collections.sort(numbers);
+        return numbers;
     }
 }
