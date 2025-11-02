40e2890eb19adb9c4a6aeee06e8e8c6a69a9f077
clainyun <dainreason0617@gmail.com>
2025-11-02 23:18:17 +0900

    feat(Application): 예외 발생 시 해당 부분부터 재입력하도록 예외 처리 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,33 +9,55 @@ import lotto.utils.LottoGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            // 1. 구입 금액 입력 및 검증
-            String inputAmount = InputView.readPurchaseAmount();
-            int purchaseAmount = InputValidator.validatePurchaseAmount(inputAmount);
 
-            // 2. 로또 발행
-            List<Lotto> purchasedLottos = LottoGenerator.generateLottos(purchaseAmount);
-            OutputView.printPurchasedLottos(purchasedLottos);
-
-            // 3. 당첨 번호 입력 및 검증
-            String winningInput = InputView.readWinningNumbers();
-            List<Integer> winningNumbers = InputParser.parseWinningNumbers(winningInput);
-
-            // 4. 보너스 번호를 입력 및 검증
-            String bonusInput = InputView.readBonusNumber();
-            int bonusNumber = InputParser.parseBonusNumber(bonusInput);
-            WinningNumbers winningLotto = new WinningNumbers(winningNumbers, bonusNumber);
-
-            // 5. 당첨 내역 계산
-            Map<Rank, Integer> results = ResultCalculator.calculateResults(purchasedLottos, winningLotto);
-
-            // 6. 결과 출력
-            OutputView.printStatistics(results);
-            double profitRate = ResultCalculator.calculateProfitRate(results, purchaseAmount);
-            OutputView.printProfitRate(profitRate);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        // 1. 구입 금액 입력 및 검증
+        int purchaseAmount;
+        while (true) {
+            try {
+                String inputAmount = InputView.readPurchaseAmount();
+                purchaseAmount = InputValidator.validatePurchaseAmount(inputAmount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
+
+        // 2. 로또 발행
+        List<Lotto> purchasedLottos = LottoGenerator.generateLottos(purchaseAmount);
+        OutputView.printPurchasedLottos(purchasedLottos);
+
+        // 3. 당첨 번호 입력 및 검증
+        List<Integer> winningNumbers;
+        while (true) {
+            try {
+                String winningInput = InputView.readWinningNumbers();
+                winningNumbers = InputParser.parseWinningNumbers(winningInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // 4. 보너스 번호 입력 및 검증
+        int bonusNumber;
+        WinningNumbers winningLotto;
+        while (true) {
+            try {
+                String bonusInput = InputView.readBonusNumber();
+                bonusNumber = InputParser.parseBonusNumber(bonusInput);
+                winningLotto = new WinningNumbers(winningNumbers, bonusNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // 5. 당첨 내역 계산
+        Map<Rank, Integer> results = ResultCalculator.calculateResults(purchasedLottos, winningLotto);
+
+        // 6. 결과 출력
+        OutputView.printStatistics(results);
+        double profitRate = ResultCalculator.calculateProfitRate(results, purchaseAmount);
+        OutputView.printProfitRate(profitRate);
     }
 }
